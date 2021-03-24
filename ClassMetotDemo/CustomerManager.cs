9f1881a8520d849434fff455165858e05a825014
ilker38 <18251966km>
2021-03-24 18:10:17 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer) {
+                Console.WriteLine("Customer Successfull Added" + "\n"
+                + "Customer Name: " + customer.FirstName+" "+customer.LastName+"\n"+"Customer Country: "+customer.Country+"\n"
+                +"Customer Identity Number: " + customer.IdentityNumber+"\n"+ "Customer ID: " + customer.Id+"\n");
+           
+
+
+
+        }
+        public void Update(Customer customer,Customer updatedCustomer) {
+
+            Console.WriteLine("******************************************************************************");
+            
+            Console.WriteLine("Customer Updated !!!" + "\n"+ "Customer Name: " + 
+                updatedCustomer.FirstName + " " + 
+                updatedCustomer.LastName + "\n" + "IdentityNumber : " + updatedCustomer.IdentityNumber +"\n" + "Country : " + updatedCustomer.Country+"\n"+ "Customer ID : "
+                + updatedCustomer.Id+"\n");
+            customer.FirstName = updatedCustomer.FirstName;
+            customer.LastName = updatedCustomer.LastName;
+            customer.IdentityNumber = updatedCustomer.IdentityNumber;
+            customer.Country = updatedCustomer.Country;
+            customer.Id = updatedCustomer.Id;
+            Console.WriteLine("******************************************************************************");
+            
+
+
+        }
+        public void Delete(Customer customer) {
+            Console.WriteLine("Customer Successfully Deleted, Deleting Customer Information is"+ "\n" + "Deleted Customer First Name : " + "" + 
+                customer.FirstName +" " +customer.LastName + "\n"+ "Deleted Customer Country : " + ""+customer.Country+"\n"+
+                "Deleted Customer Identity Number : " + ""+customer.IdentityNumber+"\n"+"Deleted Customer ID : "+customer.Id);        
+        }
+        public int topla(int sayı1, int sayı2)
+        {
+            return sayı1 + sayı2;
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            Customer customer1 = new Customer();
+
+
+            customer1.Id = 1;
+            customer1.FirstName = "ilker";
+            customer1.LastName = "Uras";
+            customer1.IdentityNumber = "1234567890";
+            customer1.Country = "Turkey";
+            customerManager.Add(customer1);
+
+            Customer customer2 = new Customer
+            { Id = 2, FirstName = "Mustafa", LastName = "Uras", IdentityNumber = "0987654321", Country = "Turkey" };
+            customerManager.Add(customer2);
+
+
+            customerManager.Update(customer1, new Customer
+            {
+                Id = 1,
+                IdentityNumber = "48596485156",
+                FirstName = customer1.FirstName,
+                LastName = customer1.LastName,
+                Country = customer1.Country
+            });
+
+
+
+            customerManager.Update(customer2, new Customer
+            {
+                Id = 2,
+                IdentityNumber = customer2.IdentityNumber,
+                FirstName = "Fatih",
+                LastName = customer2.LastName,
+                Country = customer2.Country
+        });
+
+
+            customerManager.Delete(customer1);
+            Console.WriteLine("---------------------------------");
+            customerManager.Delete(customer2);
+
+            int toplamasonucu = customerManager.topla(4, 6);
+        Console.WriteLine(toplamasonucu* 10);
+        }
+
+}
+}
